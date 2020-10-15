0449d5f1f04706ef0caae9cc44536b8f1aff0504
Robert Gawron <robert_gawron@yahoo.fr>
2020-10-15 13:47:50 +0200

    Squashed 'Software/HardwareDataLogger/' content from commit 37d90c3
    git-subtree-dir: Software/HardwareDataLogger
    git-subtree-split: 37d90c36ef6dff2428dff3a75682a6a990aea3fe

---
+++ Software/NUCLEO-F091RC/Inc/gm_display_hw.h
@@ -0,0 +1,18 @@
+#ifndef GMDISPLAY_HW_H_
+#define GMDISPLAY_HW_H_
+
+// this file wraps hardware related stuff of the display
+
+#include <stdint.h>
+
+void GMDisplayHW_Init();
+
+void GMDisplayHW_Clean();
+
+void GMDisplayHW_Update();
+
+void GMDisplayHW_DrawLine(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1);
+
+void GMDisplayHW_DrawText(uint16_t x0, uint16_t y0, char* str);
+
+#endif
---
+++ Software/NUCLEO-F091RC/Inc/gm_dose_counter.h
@@ -0,0 +1,9 @@
+#ifndef GMDOSE_COUNTER_H_
+#define GMDOSE_COUNTER_H_
+
+#include <stdint.h>
+#include "gm_measurement.h"
+
+float GMDoseCounter_Calculate(GMMeasurement_Value_t measurement);
+
+#endif
